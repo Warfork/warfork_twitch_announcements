b9d4d4319e7b7d1ba25ea303be9fa879484d7489
Warfork <51063252+Warfork@users.noreply.github.com>
2023-10-25 21:28:28 -0400

    Update twitch.py

--- twitch.py
+++ twitch.py
@@ -1,19 +1,20 @@
 import requests
+import schedule
 import time
 from datetime import datetime, timedelta
 from discord_webhook import DiscordWebhook
 
 # Settings
-
 discord_webhook_url = ""
-discord_webhook_delay = "5"
+discord_webhook_delay = 5
 game_name = ""
 twitch_client_id = ""
 twitch_client_secret = ""
 twitch_game_id = ""
-twitch_max_streams = "100"
-twitch_recheck_time = "600"
+twitch_max_streams = 100
 twitch_oauth_token = ""
+twitch_recheck_time = 600
+twitch_token_renewal_days = 28
 
 def load_announced_users(filename):
     announced_users = {}
@@ -34,9 +35,37 @@ def save_announced_users(filename, announced_users):
         for username, last_announcement_time in announced_users.items():
             file.write(f"{username},{last_announcement_time.isoformat()}\n")
 
+def get_app_access_token(client_id, client_secret):
+    url = "https://id.twitch.tv/oauth2/token"
+    params = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials"
+    }
+
+    response = requests.post(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("access_token")
+    else:
+        print(f"Failed to obtain access token. Status code: {response.status_code}")
+        return None
+
+def renew_access_token():
+    global twitch_oauth_token
+    twitch_oauth_token = get_app_access_token(twitch_client_id, twitch_client_secret)
+
 def check_for_new_users():
     announced_users = load_announced_users("announced_users.txt")
     new_users = set()
+
+    global twitch_oauth_token
+    if not twitch_oauth_token:
+        renew_access_token()
+        if not twitch_oauth_token:
+            print("Failed to obtain Twitch OAuth token. Cannot proceed.")
+            return
 
     url = "https://api.twitch.tv/helix/streams"
     params = {
@@ -71,7 +100,11 @@ def check_for_new_users():
             print(response.json()["error"])
 
 if __name__ == "__main__":
-    while True:
-        check_for_new_users()
-        time.sleep(twitch_recheck_time)
 
+    schedule.every(twitch_token_renewal_days).days.do(renew_access_token)
+
+    while True:
+
+        check_for_new_users()
+        schedule.run_pending()
+        time.sleep(twitch_recheck_time)
